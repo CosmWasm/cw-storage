443cf2e55bba6740eefcd11e7ac20e8ca39cf00f
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-01-07 18:55:17 +0100

    Update prefix.rs from cosmwasm:master (thanks webmaster128)

--- src/prefix.rs
+++ src/prefix.rs
@@ -19,19 +19,18 @@ pub struct ReadonlyPrefixedStorage<'a, T: ReadonlyStorage> {
 }
 
 impl<'a, T: ReadonlyStorage> ReadonlyPrefixedStorage<'a, T> {
-    fn new(prefix: &[u8], storage: &'a T) -> Self {
+    pub fn new(namespace: &[u8], storage: &'a T) -> Self {
         ReadonlyPrefixedStorage {
-            prefix: length_prefix(prefix),
+            prefix: key_prefix(namespace),
             storage,
         }
     }
 
-    // note: multilevel is here for demonstration purposes, but may well be removed
-    // or modified
-    #[allow(dead_code)]
-    fn multilevel(prefixes: &[&[u8]], storage: &'a T) -> Self {
+    // Nested namespaces as documented in
+    // https://github.com/webmaster128/key-namespacing#nesting
+    pub fn multilevel(namespaces: &[&[u8]], storage: &'a T) -> Self {
         ReadonlyPrefixedStorage {
-            prefix: multi_length_prefix(prefixes),
+            prefix: key_prefix_nested(namespaces),
             storage,
         }
     }
@@ -51,19 +50,18 @@ pub struct PrefixedStorage<'a, T: Storage> {
 }
 
 impl<'a, T: Storage> PrefixedStorage<'a, T> {
-    fn new(prefix: &[u8], storage: &'a mut T) -> Self {
+    pub fn new(namespace: &[u8], storage: &'a mut T) -> Self {
         PrefixedStorage {
-            prefix: length_prefix(prefix),
+            prefix: key_prefix(namespace),
             storage,
         }
     }
 
-    // note: multilevel is here for demonstration purposes, but may well be removed
-    // or modified
-    #[allow(dead_code)]
-    fn multilevel(prefixes: &[&[u8]], storage: &'a mut T) -> Self {
+    // Nested namespaces as documented in
+    // https://github.com/webmaster128/key-namespacing#nesting
+    pub fn multilevel(namespaces: &[&[u8]], storage: &'a mut T) -> Self {
         PrefixedStorage {
-            prefix: multi_length_prefix(prefixes),
+            prefix: key_prefix_nested(namespaces),
             storage,
         }
     }
@@ -85,40 +83,93 @@ impl<'a, T: Storage> Storage for PrefixedStorage<'a, T> {
     }
 }
 
-// prepend length and store this
-fn length_prefix(prefix: &[u8]) -> Vec<u8> {
-    let mut v = Vec::with_capacity(prefix.len() + 1);
-    if prefix.len() > 255 {
-        panic!("only supports prefixes up to length 255")
+// returns the length as a 2 byte big endian encoded integer
+fn len(prefix: &[u8]) -> [u8; 2] {
+    if prefix.len() > 0xFFFF {
+        panic!("only supports namespaces up to length 0xFFFF")
     }
-    v.push(prefix.len() as u8);
-    v.extend_from_slice(prefix);
-    v
+    let length_bytes = (prefix.len() as u64).to_be_bytes();
+    [length_bytes[6], length_bytes[7]]
 }
 
-// prepend length and store this
-#[allow(dead_code)]
-fn multi_length_prefix(prefixes: &[&[u8]]) -> Vec<u8> {
-    let mut size = prefixes.len();
-    for &p in prefixes {
-        size += p.len();
+// Calculates the raw key prefix for a given namespace
+// as documented in https://github.com/webmaster128/key-namespacing#length-prefixed-keys
+fn key_prefix(namespace: &[u8]) -> Vec<u8> {
+    let mut out = Vec::with_capacity(namespace.len() + 2);
+    out.extend_from_slice(&len(namespace));
+    out.extend_from_slice(namespace);
+    out
+}
+
+// Calculates the raw key prefix for a given nested namespace
+// as documented in https://github.com/webmaster128/key-namespacing#nesting
+fn key_prefix_nested(namespaces: &[&[u8]]) -> Vec<u8> {
+    let mut size = namespaces.len();
+    for &namespace in namespaces {
+        size += namespace.len() + 2;
     }
 
-    let mut v = Vec::with_capacity(size);
-    for &p in prefixes {
-        if p.len() > 255 {
-            panic!("only supports prefixes up to length 255")
-        }
-        v.push(p.len() as u8);
-        v.extend_from_slice(p);
+    let mut out = Vec::with_capacity(size);
+    for &namespace in namespaces {
+        let prefix = key_prefix(namespace);
+        out.extend_from_slice(&prefix);
     }
-    v
+    out
 }
+
 
 #[cfg(test)]
 mod test {
     use super::*;
     use cosmwasm::mock::MockStorage;
+
+    #[test]
+    fn key_prefix_works() {
+        assert_eq!(key_prefix(b""), b"\x00\x00");
+        assert_eq!(key_prefix(b"a"), b"\x00\x01a");
+        assert_eq!(key_prefix(b"ab"), b"\x00\x02ab");
+        assert_eq!(key_prefix(b"abc"), b"\x00\x03abc");
+    }
+
+    #[test]
+    fn key_prefix_works_for_long_prefix() {
+        let long_namespace1 = vec![0; 256];
+        let prefix1 = key_prefix(&long_namespace1);
+        assert_eq!(prefix1.len(), 256 + 2);
+        assert_eq!(&prefix1[0..2], b"\x01\x00");
+
+        let long_namespace2 = vec![0; 30000];
+        let prefix2 = key_prefix(&long_namespace2);
+        assert_eq!(prefix2.len(), 30000 + 2);
+        assert_eq!(&prefix2[0..2], b"\x75\x30");
+
+        let long_namespace3 = vec![0; 0xFFFF];
+        let prefix3 = key_prefix(&long_namespace3);
+        assert_eq!(prefix3.len(), 0xFFFF + 2);
+        assert_eq!(&prefix3[0..2], b"\xFF\xFF");
+    }
+
+    #[test]
+    #[should_panic(expected = "only supports namespaces up to length 0xFFFF")]
+    fn key_prefix_panics_for_too_long_prefix() {
+        let limit = 0xFFFF;
+        let long_namespace = vec![0; limit + 1];
+        key_prefix(&long_namespace);
+    }
+
+    #[test]
+    fn key_prefix_nested_works() {
+        assert_eq!(key_prefix_nested(&[]), b"");
+        assert_eq!(key_prefix_nested(&[b""]), b"\x00\x00");
+        assert_eq!(key_prefix_nested(&[b"", b""]), b"\x00\x00\x00\x00");
+
+        assert_eq!(key_prefix_nested(&[b"a"]), b"\x00\x01a");
+        assert_eq!(key_prefix_nested(&[b"a", b"ab"]), b"\x00\x01a\x00\x02ab");
+        assert_eq!(
+            key_prefix_nested(&[b"a", b"ab", b"abc"]),
+            b"\x00\x01a\x00\x02ab\x00\x03abc"
+        );
+    }
 
     #[test]
     fn prefix_safe() {
@@ -162,25 +213,5 @@ mod test {
         let a = ReadonlyPrefixedStorage::new(b"foo", &storage);
         let b = ReadonlyPrefixedStorage::new(b"bar", &a);
         assert_eq!(Some(b"time".to_vec()), b.get(b"second"));
-    }
-
-    // demo
-    #[test]
-    fn foo() {
-        use cw_storage::{prefixed, prefixed_ro};
-
-        let mut store = MockStorage::new();
-
-        let mut foos = prefixed(b"foo", &mut store);
-        foos.set(b"one", b"foo");
-
-        let mut bars = prefixed(b"bar", &mut store);
-        bars.set(b"one", b"bar");
-
-        let read_foo = prefixed_ro(b"foo", &store);
-        assert_eq!(b"foo".to_vec(), read_foo.get(b"one").unwrap());
-
-        let read_bar = prefixed_ro(b"bar", &store);
-        assert_eq!(b"bar".to_vec(), read_bar.get(b"one").unwrap());
     }
 }
